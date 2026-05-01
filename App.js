e0f924a7435bc9da711006b62198ca83bbb1d138
Tanja Vatka <tanja.vatka@hotmail.com>
2026-05-01 19:28:28 +0300

    first commit + bottom nav bar done.

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import TodayScreen from './TodayScreen';
+import SettingScreen from './SettingScreen';
+import TomorrowScreen from './TomorrowScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Tab.Navigator
+        ScreenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+
+            let iconName;
+
+            if (route.name === 'Today') {
+              iconName = 'thunderbolt'
+            } else if (route.name === 'Tomorrow') {
+              // iconName = 'electric-bolt' 'zap' 'bolt'
+              iconName = 'flash';
+            } else if (route.name === 'Settings') {
+              iconName = 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Today" component={TodayScreen} />
+        <Tab.Screen name="Tomorrow" component={TomorrowScreen} />
+        <Tab.Screen name="Settings" component={SettingScreen} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
