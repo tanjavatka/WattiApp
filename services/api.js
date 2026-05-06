c4616b737cef57d2d8718bfaffd86c52bc5a5acf
Tanja Vatka <tanja.vatka@hotmail.com>
2026-05-06 07:52:11 +0300

    testattu datan toimivuutta ja lisätty kommentteja.

--- services/api.js
+++ services/api.js
@@ -1,29 +1,40 @@
 
+// Haetaan sähkön hinnat päivän mukaan | date = today / tomorrow
 const fetchPricesByDate = async (date) => {
   try {
     const response = await fetch(
       `https://www.sahkohinta-api.fi/api/vartti/v1/halpa?vartit=96&tulos=haja&aikaraja=${date}`
     );
 
+    // koodin testausta
+    // const text = await response.text();
+    // console.log("RAW RESPONSE:", text);
+
     const data = await response.json();
 
     return data;
+
   } catch (error) {
     console.log(error);
     return [];
   }
 };
 
+// Haetaan tämän päivän hinnat
 export const handleFetchTodayPrices = () => {
+
+  // haetaan suomen aikojen mukaan
   const today = new Date().toLocaleDateString("sv-SE");
 
   return fetchPricesByDate(today);
 };
 
+// Haetaan huomisen päivän hinnat
 export const handleFetchTomorrowPrices = () => {
   const tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
 
+  // haetaan suomen aikojen mukaan
   const tomorrowDate = tomorrow.toLocaleDateString("sv-SE");
 
   return fetchPricesByDate(tomorrowDate);
