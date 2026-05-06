462a87892928862581f0f0309c0ab522ca4b66d0
Tanja Vatka <tanja.vatka@hotmail.com>
2026-05-06 08:30:56 +0300

    lisätty väriteema tumma/vaalea. Muokattu navigointia myös PriceScreenin mukaan.

--- navigation/NavigatorApp.js
+++ navigation/NavigatorApp.js
@@ -2,18 +2,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-import TodayScreen from '../screens/TodayScreen';
-import TomorrowScreen from '../screens/TomorrowScreen';
+import PriceScreen from '../screens/PriceScreen';
 import SettingScreen from '../screens/SettingScreen';
 
+// Alareunan navigaatio
 const Tab = createBottomTabNavigator();
 
-export default function NavigatorApp() {
+export default function NavigatorApp({ theme, isDark, setIsDark }) {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           headerShown: false,
+
+          tabBarStyle: {
+            backgroundColor: theme.card,
+          },
+
+          tabBarActiveTintColor: theme.text,
+          tabBarInactiveTintColor: "#888",
+
           tabBarIcon: ({ color, size }) => {
             let iconName;
 
@@ -29,12 +37,34 @@ export default function NavigatorApp() {
           },
         })}
       >
-        <Tab.Screen name="Tänään" component={TodayScreen} />
-        <Tab.Screen name="Huomenna" component={TomorrowScreen} />
-        <Tab.Screen name="Asetukset" component={SettingScreen} />
+
+        {/* TODAY */}
+        <Tab.Screen name="Tänään">
+          {(props) => (
+            <PriceScreen {...props} mode="today" theme={theme} />
+          )}
+        </Tab.Screen>
+
+        {/* TOMORROW */}
+        <Tab.Screen name="Huomenna">
+          {(props) => (
+            <PriceScreen {...props} mode="tomorrow" theme={theme} />
+          )}
+        </Tab.Screen>
+
+        {/* SETTINGS */}
+        <Tab.Screen name="Asetukset" >
+          {(props) => (
+            <SettingScreen
+              {...props}
+              theme={theme}
+              isDark={isDark}
+              setIsDark={setIsDark}
+            />
+          )}
+        </Tab.Screen>
+
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-// muuta ikonien väriä !
